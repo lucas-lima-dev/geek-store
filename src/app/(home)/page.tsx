bc14d581994cbf71393b215df15883e8e8198e31
Lucas Lima <lucas.sdslima@gmail.com>
2023-10-18 19:01:06 -0300

    feat: add Categories and CategoriesItem component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,25 @@
 "use client";
 
-import { useSession } from 'next-auth/react';
-import Image from 'next/image';
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import Categories from "./components/categories";
 
 export default function Home() {
-  const {data} = useSession()
+  const { data } = useSession();
 
   return (
-    <div className='p-5'>
-     <Image
-     src="/banner-home-01.png"
-     height={0}
-      width={0}
-      className='h-auto w-full'
-      sizes='100vw'
-      alt='Até 55% de desconto esse mes!'
-     />
-
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto esse mes!"
+      />
+      <div className="mt-8">
+        <Categories />
+      </div>
     </div>
-  )
+  );
 }
