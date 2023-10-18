ae8a3f4a74ccb585a4d7fefb90fafe585d6ff37c
Lucas Lima <lucas.sdslima@gmail.com>
2023-10-18 11:05:15 -0300

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+
+export default function Home() {
+  const {data} = useSession()
+
+  return (
+    <div className='p-5'>
+     <Image
+     src="/banner-home-01.png"
+     height={0}
+      width={0}
+      className='h-auto w-full'
+      sizes='100vw'
+      alt='Até 55% de desconto esse mes!'
+     />
+
+    </div>
+  )
+}
