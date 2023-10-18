59aff7847099a5883876b5daa713a043e24757c2
Lucas Lima <lucas.sdslima@gmail.com>
2023-10-18 20:51:27 -0300

    feat: add third promo banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -21,16 +21,16 @@ export default async function Home() {
     },
   });
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <PromoBanner
         src="/banner-home-01.png"
         alt="Até 55% de desconto esse mes!"
       />
-      <div className="mt-8 px-5">
+      <div className=" px-5">
         <Categories />
       </div>
 
-      <div className="mt-8">
+      <div >
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
@@ -40,9 +40,16 @@ export default async function Home() {
         alt="Até 55% de desconto em mouses!"
       />
 
-      <div className="mt-8">
+      <div >
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
+      </div>
+
+      <div >
+      <PromoBanner
+        src="/banner-home-03.png"
+        alt="Até 55% de desconto em mouses!"
+      />
       </div>
     </div>
   );
