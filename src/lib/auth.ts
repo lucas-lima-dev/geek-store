c5f6fff2a6b0122f33280fdf38c7ca92c261c18e
Lucas Lima <lucas.sdslima@gmail.com>
2023-10-21 11:52:56 -0300

    fix: nextauth secret syntax correction

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
+import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
-import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient),
