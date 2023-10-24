89778436725be05ae8158e88e00352cab5969819
Lucas Lima <lucas.sdslima@gmail.com>
2023-10-24 19:38:38 -0300

    feat: add createOrder server action

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
-import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
 
@@ -11,4 +11,15 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user = { ...session.user, id: user.id } as {
+        id: string;
+        name: string;
+        email: string;
+      };
+
+      return session;
+    },
+  },
 };
