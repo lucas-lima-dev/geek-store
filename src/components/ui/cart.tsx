733913731ae5af5073ab1ff9d33fa615c4ae78da
Lucas Lima <lucas.sdslima@gmail.com>
2023-10-24 20:13:39 -0300

    feat: ensure order is updated on checkout success

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -4,28 +4,32 @@ import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/product";
-import { Separator } from "@radix-ui/react-separator";
+import { Separator } from "./separator";
 import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
 import { createCheckout } from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
-import { useSession } from "next-auth/react";
 import { createOrder } from "@/actions/order";
+import { useSession } from "next-auth/react";
 
 const Cart = () => {
   const { data } = useSession();
 
-  const { products, total, subTotal, totalDiscount } = useContext(CartContext);
+  const { products, total, totalDiscount } = useContext(CartContext);
 
   const handleFinishPurchaseClick = async () => {
-    if (!data?.user)
-      return alert("Você precisa estar logado para finalizar a compra");
+    if (!data?.user) {
+      // TODO: redirecionar para o login
+      return;
+    }
 
-    await createOrder(products, (data?.user as any).id);
+    const order = await createOrder(products, (data?.user as any).id);
 
-    const checkout = await createCheckout(products);
+    const checkout = await createCheckout(products, order.id);
 
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    // Criar pedido no banco
 
     stripe?.redirectToCheckout({
       sessionId: checkout.id,
@@ -42,6 +46,7 @@ const Cart = () => {
         Carrinho
       </Badge>
 
+      {/* RENDERIZAR OS PRODUTOS */}
       <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
         <ScrollArea className="h-full">
           <div className="flex h-full flex-col gap-8">
@@ -54,40 +59,41 @@ const Cart = () => {
               ))
             ) : (
               <p className="text-center font-semibold">
-                Você ainda não tem nenhum produto no carrinho
+                Carrinho vazio. Vamos fazer compras?
               </p>
             )}
           </div>
         </ScrollArea>
       </div>
+
       {products.length > 0 && (
         <div className="flex flex-col gap-3">
           <Separator />
 
+          {/* <div className="flex items-center justify-between text-xs">
+            <p>Subtotal</p>
+            <p>R$ {subtotal.toFixed(2)}</p>
+          </div> */}
+
+          <Separator />
+
           <div className="flex items-center justify-between text-xs">
-            <p className="">Subtotal</p>
-            <p className="">R$ {subTotal.toFixed(2)}</p>
+            <p>Entrega</p>
+            <p>GRÁTIS</p>
           </div>
 
           <Separator />
 
           <div className="flex items-center justify-between text-xs">
-            <p className="">Entrega</p>
-            <p className="">GRÁTIS</p>
-          </div>
-
-          <Separator />
-
-          <div className="flex items-center justify-between text-xs">
-            <p className="">Descontos</p>
-            <p className=""> - R$ {totalDiscount.toFixed(2)}</p>
+            <p>Descontos</p>
+            <p>- R$ {totalDiscount.toFixed(2)}</p>
           </div>
 
           <Separator />
 
           <div className="flex items-center justify-between text-sm font-bold">
-            <p className="">Total</p>
-            <p className="">R$ {total.toFixed(2)}</p>
+            <p>Total</p>
+            <p>R$ {total.toFixed(2)}</p>
           </div>
 
           <Button
