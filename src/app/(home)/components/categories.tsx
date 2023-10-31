498b8b36142d87dc102bdcd3d181e26da76f9798
Lucas Lima <lucas.sdslima@gmail.com>
2023-10-31 09:11:43 -0300

    feat: ensure categories are responsive for desktop

--- src/app/(home)/components/categories.tsx
+++ src/app/(home)/components/categories.tsx
@@ -5,7 +5,7 @@ const Categories = async () => {
     const categories = await prismaClient.category.findMany({});
 
     return ( 
-        <div className="grid grid-cols-2 gap-y-2 gap-x-4">
+        <div className="grid grid-cols-2 gap-y-2 gap-x-4 lg:grid-cols-6">
             {categories.map((category) => (
                 <CategoryItem key={category.id} category={category} />
             ))}
