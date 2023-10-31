13662b468bee04cfc0eb3c7d5ee9abb7cc665563
Lucas Lima <lucas.sdslima@gmail.com>
2023-10-31 11:10:07 -0300

    feat: ensure product details page is responsive on desktop view

--- src/app/product/[slug]/components/product-info.tsx
+++ src/app/product/[slug]/components/product-info.tsx
@@ -29,18 +29,20 @@ const ProductInfo = ({ product }: ProductInfoProps) => {
   };
 
   return (
-    <div className="flex flex-col px-5">
-      <h2 className="text-lg">{product.name}</h2>
+    <div className="flex flex-col px-5 lg:w-[40%] lg:rounded-lg lg:bg-accent lg:p-10">
+      <h2 className="g:text-2xl text-lg">{product.name}</h2>
       <div className="flex items-center gap-2">
-        <h1 className="text-xl font-bold">
+        <h1 className="text-xl font-bold lg:text-3xl">
           R$ {product.totalPrice.toFixed(2)}
         </h1>
         {product.discountPercentage > 0 && (
-          <DiscoundBadge>{product.discountPercentage}</DiscoundBadge>
+          <DiscoundBadge className="lg:text-base">
+            {product.discountPercentage}
+          </DiscoundBadge>
         )}
       </div>
       {product.discountPercentage > 0 && (
-        <p className="text-sm line-through opacity-75">
+        <p className="text-sm line-through opacity-75 lg:text-base">
           R$ {Number(product.basePrice).toFixed(2)}
         </p>
       )}
@@ -77,7 +79,7 @@ const ProductInfo = ({ product }: ProductInfoProps) => {
         Adicionar ao carrinho
       </Button>
 
-      <div className="mt-5 flex items-center justify-between rounded-lg bg-accent px-5 py-2">
+      <div className="mt-5 flex items-center justify-between rounded-lg bg-accent px-5 py-2 lg:bg-[#2A2A2A]">
         <div className="flex items-center gap-2">
           <TruckIcon />
           <div className="flex flex-col">
